4d853821a978abe0256548a15149e921c2933bda
fai-sas <craftedbyfaisal@gmail.com>
2023-09-24 21:46:58 +0600

    rendered cards on homepage

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,4 +1,20 @@
-const Card = () => {
-  return <div>SingleCard</div>
+const Card = ({ card }) => {
+  const { picture, category, title, card_bg, category_bg, text_color } = card
+
+  return (
+    <div
+      className='shadow-xl card card-compact'
+      style={{ backgroundColor: category_bg, color: text_color }}
+    >
+      <img src={picture} alt={title} />
+
+      <div className='card-body'>
+        <p className='text-sm' style={{ backgroundColor: card_bg }}>
+          {category}
+        </p>
+        <p className='text-xl font-semibold'>{title}</p>
+      </div>
+    </div>
+  )
 }
 export default Card
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from 'react'
+import Card from './Card'
+
 const Cards = () => {
-  return <div>Cards</div>
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    fetch('donations.json')
+      .then((response) => response.json())
+      .then((data) => setCards(data))
+  }, [])
+
+  return (
+    <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-4'>
+      {cards.map((card) => {
+        return <Card key={card.id} card={card}></Card>
+      })}
+    </div>
+  )
 }
 export default Cards
