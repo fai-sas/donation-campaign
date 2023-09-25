44988b19913d7d416dbc709d4812341b9ae311cc
fai-sas <craftedbyfaisal@gmail.com>
2023-09-25 12:05:33 +0600

    dynamic button color added for single donation details page

--- src/components/SingleDonation/SingleDonation.jsx
+++ src/components/SingleDonation/SingleDonation.jsx
@@ -3,9 +3,7 @@ import { saveDonationData } from '../../utils/localStorage'
 
 const SingleDonation = () => {
   const data = useLoaderData()
-  console.log(data)
-  // console.log(data[0].category_bg)
-  const { id, category_bg, text_color } = useParams()
+  const { id } = useParams()
   const donation = data.find((donation) => donation.id === id)
 
   const handleDonation = () => {
@@ -15,22 +13,27 @@ const SingleDonation = () => {
 
   return (
     <div className='container py-12 mx-auto'>
-      <div>
+      <div className='relative'>
         <img src={donation.picture} className='object-cover w-full ' alt='' />
-        <button
-          onClick={handleDonation}
-          className='btn'
-          // style={{ backgroundColor: category_bg, color: text_color }}
-        >
-          Donate ${donation.price}
-        </button>
-        <article className='py-8'>
-          <h1 className='text-4xl font-bold'>{donation.title}</h1>
-          <p className='py-4 text-[#0b0b0bb2] leading-7'>
-            {donation.description}
-          </p>
-        </article>
+        <div className='absolute bottom-0 left-0 w-full p-8  bg-[#0b0b0b7f] '>
+          <button
+            onClick={handleDonation}
+            className='text-xl font-semibold text-white border-none opacity-100 btn'
+            style={{
+              backgroundColor: donation.text_color,
+            }}
+          >
+            Donate ${donation.price}
+          </button>
+        </div>
       </div>
+
+      <article className='py-8'>
+        <h1 className='text-4xl font-bold'>{donation.title}</h1>
+        <p className='py-4 text-[#0b0b0bb2] leading-7'>
+          {donation.description}
+        </p>
+      </article>
     </div>
   )
 }
